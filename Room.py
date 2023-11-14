2825380f7e75c7f1d4ccd96b50bcd73e86efc367
Hi-Im-Jony <jccicai@yahoo.ie>
2023-11-14 14:50:39 +0000

    added apparatus to rooms

--- Room.py
+++ Room.py
@@ -4,6 +4,7 @@ Room class
 from random import uniform
 from time import sleep
 from Device import Device
+from Apparatus import Apparatus
 
 class Room:
     def __init__(self, room_id):
@@ -15,13 +16,20 @@ class Room:
             "CO2": 400,         # CO2 level in ppm (parts per million)
             "radiation": 0.2,   # Radiation level in microsieverts per hour (uSv/h)
             "motion": 0,        # Whether or not something is moving in the room (0 or 1)
-            "heater": 0,        # Whether or not heater is on
-            "light": 0,         # Whether or not light is on
+            "light": 0,         # Light level in lux
+        }
+        self.apparatus = {
+            "heater": Apparatus(room_id, "heater", "temp", "increase_by", 0.1),
+            "lights": Apparatus(room_id, "lights", "light", "set_to", 100 ),
+            "AC": Apparatus(room_id, "AC", "temp", "decrease_by", 0.05)
         }
 
     def simulate(self):
         # simulate a change in the room stats
         while True:
+            # Update room stats via room apparatus that is on
+            self.update_via_apparatus()
+            
             # Simulate temperature fluctuation within a realistic range
             self.stats["temp"] += uniform(-0.01, 0.01)
             self.stats["temp"] = min(max(self.stats["temp"], -10), 40)
@@ -41,10 +49,26 @@ class Room:
             # Simulate radiation slightly fluctuating
             self.stats["radiation"] += uniform(-0.05, 0.05)
             self.stats["radiation"] = min(max(self.stats["radiation"], 0), 0.5)
+            sleep(1)   
 
-            # todo find a good way to adjust motion
+            # Todo, Simulate motion 
 
-            sleep(1)    
+    def update_via_apparatus(self):
+        for _, app in self.apparatus.items():
+            if app.on and app.affected_stat in self.stats.keys():
+                if app.change_type == "set_to":
+                    self.stats[app.affected_stat] = app.change_amount
+                elif app.change_type == "increase_by":
+                    self.stats[app.affected_stat] += app.change_amount
+                elif app.change_type == "decrease_by":
+                    self.stats[app.affected_stat] -= app.change_amount
+
+    def toggle_value(self, value):
+        if value in ["motion", "heater", "light"]:
+            self.stats[value] = 1 if self.stats[value] == 0 else 0
+
+    def set_apparatus(self, apparatus: Apparatus):
+        self.apparatus[apparatus.apparatus_type] = apparatus
 
     def main(self):
         self.device.turn_on()
