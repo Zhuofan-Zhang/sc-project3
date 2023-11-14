045e69887d63f56be942e5c8859eaa4a84abfaba
Hi-Im-Jony <jccicai@yahoo.ie>
2023-11-14 14:15:55 +0000

    updated device

--- Device.py
+++ Device.py
@@ -11,45 +11,89 @@ class Device:
         self.registry_port = registry_port
         self.device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.device_socket.connect((self.registry_host, self.registry_port))
-        self.room = room
-        self.sensors = [self.DeviceSensor(device_id, sens_type) for sens_type in SENSOR_TYPES]
-        self.register()
+        self._register()
+        # default triggers
+        self._triggers = {
+            "temp": ("<->", (19, 22)),       # Temp in degrees Celsius
+            "humidity": ("<->",(0.4, 0.45)), # Humidity percentage as decimal
+            "CO": ("<", 65),                 # CO level in ppm (parts per million)
+            "CO2": ("<", 600),               # CO2 level in ppm (parts per million)
+            "radiation": ("<", 15),          # Radiation level in microsieverts per hour (uSv/h)
+        }
+        self._room = room
+        self._sensors = [self.DeviceSensor(device_id, sens_type, self._triggers[sens_type] if sens_type in self._triggers else None) 
+                         for sens_type in SENSOR_TYPES]
+        self._on = False
 
-    def register(self):
-        message = json.dumps({'type': 'register', 'device_id': self.device_id})
+    def _register(self):
+        message = json.dumps({'type': '_register', 'device_id': self.device_id})
         self.device_socket.sendall(message.encode('utf-8'))
 
-    def listen_for_data(self):
-        while True:
+    def _listen_for_data(self):
+        while self._on:
             data = self.device_socket.recv(1024)
             if data:
                 print(f"Received data: {data.decode('utf-8')}")
 
-    def run_sensors(self):
-        while True:
+    def _run_sensors(self):
+        while self._on:
             for sensor in self.sensors:
-                sensor.emit(sensor.get_reading(self.room))
+                sensor.sense(self._room)
+
+    def update_trigger(self, target, trigger):
+        if target in self._triggers:
+            self._triggers[target] = trigger
+            self._sensors[target].update_trigger(trigger)
 
     def turn_on(self):
-        threads = {"lt": threading.Thread(target=self.listen_for_data), # Listening Thread
-                   "rt": threading.Thread(target=self.run_sensors)}     # Sensor Thread
+        self._on = True
+        threads = {"lt": threading.Thread(target=self._listen_for_data), # Listening Thread
+                   "rt": threading.Thread(target=self._run_sensors)}     # Sensor Thread
         for _ , t in threads.items():
             t.daemon = True
             t.start()
 
+    def turn_off(self):
+        self._on = False
+
 
     class DeviceSensor:
-        def __init__(self, device_id, sensor_type):
+        def __init__(self, device_id, sensor_type, trigger):
             self.name = device_id + '@' + sensor_type
             self.sensor_type = sensor_type
+            self.trigger = trigger
             self.last_reading = None
+            self.trigger_function = None
 
-        def get_reading(self, room):
+        def update_trigger(self, trigger):
+            self.trigger = trigger
+
+        def update_trigger_function(self, trigger_function):
+            self.trigger_function = trigger_function
+
+        def _get_reading(self, room):
             return round(room.stats[self.sensor_type], 3)
         
-        def emit(self, new_reading):
+        def _check_trigger(self, reading):
+            trigger_condition, trigger_value = self.trigger
+            if trigger_condition == "<" and reading < trigger_value:
+                self.trigger_function(reading)
+            elif trigger_condition == ">" and reading > trigger_value:
+                self.trigger_function(reading)
+            elif trigger_condition == "<->":
+                val_a, val_b = trigger_value
+                if val_a < reading or reading > val_b:
+                    self.trigger_function(reading)
+        
+        def _emit(self, new_reading):
             # Todo, implement actual emit logic
-            if new_reading != self.last_reading:
-                print(f"{self.name}: {new_reading}")
-                self.last_reading = new_reading
+            print(f"{self.name}: {new_reading}")
+            self.last_reading = new_reading
+            
+        def sense(self, room):
+            reading = self._get_reading(room)
+            if reading != self.last_reading:
+                self._check_trigger(reading)
+                self._emit(reading)
+            
 
